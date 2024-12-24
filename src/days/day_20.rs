dd3f20959c4b4770b40eeede97657bb356998aff
lsprgabriel <ogabrielpereira@pm.me>
2024-12-24 15:05:13 -0300

    Day 21 done

--- src/days/day_20.rs
+++ src/days/day_20.rs
@@ -1,37 +1,52 @@
 use std::marker::PhantomData;
-
 pub struct Empty;
 pub struct Ready;
 pub struct Flying;
-
 // TODO: Define the `status` method for all states
-
-pub struct Sleigh<T> {
+pub trait Status {
+    fn status() -> &'static str;
+}
+impl Status for Empty {
+    fn status() -> &'static str {
+        "Empty"
+    }
+}
+impl Status for Ready {
+    fn status() -> &'static str {
+        "Ready"
+    }
+}
+impl Status for Flying {
+    fn status() -> &'static str {
+        "Flying"
+    }
+}
+pub struct Sleigh<T: Status> {
     // This is only public for testing purposes
     // In real-world scenarios, this should be private
     pub state: PhantomData<T>,
 }
-
+impl<T: Status> Sleigh<T> {
+    pub fn status(&self) -> &'static str {
+        T::status()
+    }
+}
 impl Sleigh<Empty> {
     pub fn new() -> Self {
         Self { state: PhantomData }
     }
-
     pub fn load(self) -> Sleigh<Ready> {
         Sleigh { state: PhantomData }
     }
 }
-
 impl Sleigh<Ready> {
     pub fn take_off(self) -> Sleigh<Flying> {
         Sleigh { state: PhantomData }
     }
-
     pub fn unload(self) -> Sleigh<Empty> {
         Sleigh { state: PhantomData }
     }
 }
-
 impl Sleigh<Flying> {
     pub fn land(self) -> Sleigh<Ready> {
         Sleigh { state: PhantomData }
